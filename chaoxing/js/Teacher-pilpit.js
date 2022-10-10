11f59ce255cb1fb2982667edf3edd4b0b412ccaf
fe-intern <fe-intern@lenovocloud.com>
2022-10-11 07:57:37 +0800

    Site updated: 2022-10-11 07:57:35

--- chaoxing/js/Teacher-pilpit.js
+++ chaoxing/js/Teacher-pilpit.js
@@ -6,12 +6,4 @@ var tab2=document.getElementById("teacher-demo2");
 tab2.innerHTML=tab1.innerHTML; //用于让tab1的内容与tab2的内容一样
 function Marquee(){
 //当元素要显示tab2的内容时，让他去显示tab1的内容，因为tab1与tab2的内容一样，就实现了滚动，因为速度太快了，所以看上去就是连续的
-	if(tab2.offsetWidth-tab.scrollLeft&lt;=0)
-		tab.scrollLeft-=tab1.offsetWidth 
-		else{
-			tab.scrollLeft++;
-		}
-	}
-var MyMar=setInterval(Marquee,speed);
-tab.onmouseover=function() {clearInterval(MyMar)};
-tab.onmouseout=function() {MyMar=setInterval(Marquee,speed)};
+	if(tab2.offsetWidth-tab.scrollLeft
